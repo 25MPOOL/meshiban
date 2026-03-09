d10573ccf630d4c65415767f2896fe3632ca2054
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-09 23:09:53 +0900

    fix: fmt

--- src/main.py
+++ src/main.py
@@ -9,13 +9,16 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
     await tree.sync()
 
+
 @tree.command(name="ping", description="ping! pong!")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("pong!")
+
 
 client.run(os.getenv("DISCORD_TOKEN"))
